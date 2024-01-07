c103e0d1d29c285d80ffc3ec3a8629a495dfc0df
Valentin Ferreyra <valen.ferreyra@outlook.com>
2024-01-07 16:32:19 -0300

    add user schema

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = Schema({
+    name: {
+        type: String,
+        required: [true, 'name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'email is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'password is required']
+    },
+    img: {
+        type: String
+    },
+    role: {
+        type: String,
+        required: true,
+        emun: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    createdFromGoogle: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = model('User', UserSchema);
